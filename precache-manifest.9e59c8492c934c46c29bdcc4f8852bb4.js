fd0698d05a92c0385dd02dc64958ecacfe292b4e
xvjieru <1320836265@qq.com>
2020-10-18 14:59:53 +0800

    2

---
+++ precache-manifest.9e59c8492c934c46c29bdcc4f8852bb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d5f3d8b2cbe6d2527a67e89b67bf5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d300e6ce539ec5512a2d",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "6bb0fc6425affa96a4f1",
+    "url": "/static/css/main.d925e930.chunk.css"
+  },
+  {
+    "revision": "d300e6ce539ec5512a2d",
+    "url": "/static/js/2.75f7eae0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.75f7eae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb0fc6425affa96a4f1",
+    "url": "/static/js/main.1756bfca.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
